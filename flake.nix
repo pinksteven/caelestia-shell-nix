ae2404581e318bed10a82c27fecf0ebc59185793
Steven <st.latuszek@gmail.com>
2025-07-10 16:05:00 +0200

    package along with caelestia-cli

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    caelestia-cli = {
+      url = "github:caelestia-dots/cli";
+      flake = false;
+    };
   };
 
   outputs =
@@ -16,6 +20,7 @@
       nixpkgs,
       flake-utils,
       quickshell,
+      caelestia-cli,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -24,94 +29,135 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-
-        deps = with pkgs; [
-          app2unit
-          aubio
-          bluez
-          brightnessctl
-          cava
-          curl
-          ddcutil
-          fishMinimal
-          grim
-          inotify-tools
-          libqalculate
-          lm_sensors
-          material-symbols
-          networkmanager
-          pipewire
-          power-profiles-daemon
-          procps
-          kdePackages.qtdeclarative
-          quickshell.packages.${system}.default
-          swappy
-        ];
-
       in
       {
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "caelestia-shell";
-          src = ./.;
-          version = self.shortRev or "dirty";
+        packages = {
+          shell =
+            let
+              deps = with pkgs; [
+                app2unit
+                aubio
+                bluez
+                brightnessctl
+                cava
+                curl
+                ddcutil
+                fishMinimal
+                grim
+                inotify-tools
+                libqalculate
+                lm_sensors
+                material-symbols
+                networkmanager
+                pipewire
+                power-profiles-daemon
+                procps
+                kdePackages.qtdeclarative
+                quickshell.packages.${system}.default
+                swappy
+              ];
+            in
+            pkgs.stdenv.mkDerivation {
+              pname = "caelestia-shell";
+              src = ./.;
+              version = self.shortRev or "dirty";
 
-          nativeBuildInputs = with pkgs; [
-            gcc
-            makeWrapper
-          ];
+              nativeBuildInputs = with pkgs; [
+                gcc
+                makeWrapper
+              ];
 
-          buildInputs = deps;
-          dontWrapQtApps = true;
+              buildInputs = deps;
+              dontWrapQtApps = true;
 
-          buildPhase = ''
-            runHook preBuild
+              buildPhase = ''
+                runHook preBuild
 
-            mkdir -p bin
-            g++ -std=c++17 -Wall -Wextra \
-            -I${pkgs.pipewire.dev}/include/pipewire-0.3 \
-            -I${pkgs.pipewire.dev}/include/spa-0.2 \
-            -I${pkgs.aubio}/include/aubio \
-            assets/beat_detector.cpp \
-            -o bin/beat_detector \
-            -lpipewire-0.3 -laubio
+                mkdir -p bin
+                g++ -std=c++17 -Wall -Wextra \
+                -I${pkgs.pipewire.dev}/include/pipewire-0.3 \
+                -I${pkgs.pipewire.dev}/include/spa-0.2 \
+                -I${pkgs.aubio}/include/aubio \
+                assets/beat_detector.cpp \
+                -o bin/beat_detector \
+                -lpipewire-0.3 -laubio
 
-            runHook postBuild
-          '';
+                runHook postBuild
+              '';
 
-          installPhase = ''
-            runHook preInstall
+              installPhase = ''
+                runHook preInstall
 
-            mkdir -p $out/bin
-            mkdir -p $out/share/quickshell
-            mkdir -p $out/share/fonts
+                mkdir -p $out/bin
+                mkdir -p $out/share/quickshell
+                mkdir -p $out/share/fonts
 
-            cp -r assets config modules services utils widgets $out/share/quickshell/
-            cp -r ${pkgs.material-symbols}/share/fonts/* $out/share/fonts/
-            cp shell.qml $out/share/quickshell/
-            cp bin/beat_detector $out/bin/
-            cp run.fish $out/bin/
-            chmod +x $out/bin/run.fish
+                cp -r assets config modules services utils widgets $out/share/quickshell/
+                cp -r ${pkgs.material-symbols}/share/fonts/* $out/share/fonts/
+                cp shell.qml $out/share/quickshell/
+                cp bin/beat_detector $out/bin/
 
-            # Passing entire qs command via the script
-            sed -i "/qs/i \
-            if count \$argv > \/dev\/null \
-            \n  qs -p $out\/share\/quickshell \$argv \
-            \nelse" $out/bin/run.fish
+                runHook postInstall
+              '';
 
-            sed -i "/\$cache/a end" $out/bin/run.fish
+              postFixup = ''
+                makeWrapper ${quickshell.packages.${system}.default}/bin/qs $out/bin/caelestia-shell \
+                --add-flags "-p $out/share/quickshell" \
+                --set QT_QUICK_CONTROLS_STYLE Basic \
+                --set CAELESTIA_BD_PATH "$out/bin/beat_detector" \
+                --prefix PATH : ${pkgs.lib.makeBinPath deps} \
+                --prefix FONTCONFIG_PATH : ${pkgs.fontconfig.out}/etc/fonts
+              '';
+            };
+          cli =
+            let
+              deps = with pkgs; [
+                libnotify
+                swappy
+                grim
+                dart-sass
+                app2unit
+                wl-clipboard
+                slurp
+                wl-screenrec
+                libpulseaudio
+                cliphist
+                fuzzel
+                killall
+                python3Packages.hatch-vcs
+                python3Packages.pillow
+                python3Packages.materialyoucolor
+              ];
+            in
+            pkgs.python3Packages.buildPythonPackage {
+              pname = "caelestia-cli";
+              src = caelestia-cli;
+              version = self.shortRev or "000000";
+              pyproject = true;
 
-            substituteInPlace $out/bin/run.fish --replace-quiet "qs -p (dirname (status filename))" "  qs -p $out/share/quickshell"
+              build-system = with pkgs.python3Packages; [
+                hatchling
+              ];
 
-            runHook postInstall
-          '';
+              patchPhase = ''
+                chmod +w src/caelestia/utils/version.py
+                chmod +w src/caelestia/subcommands/shell.py
+                chmod +w src/caelestia/subcommands/screenshot.py
+                substituteInPlace src/caelestia/utils/version.py --replace-quiet "qs" "caelestia-shell";
+                substituteInPlace src/caelestia/subcommands/shell.py --replace-quiet "\"qs\", \"-n\", \"-c\", \"caelestia\"" "\"caelestia-shell\", \"-n\"";
+                substituteInPlace src/caelestia/subcommands/shell.py --replace-quiet "\"qs\", \"-c\", \"caelestia\"" "\"caelestia-shell\"";
+                substituteInPlace src/caelestia/subcommands/screenshot.py --replace-quiet "\"qs\", \"-c\", \"caelestia\"" "\"caelestia-shell\"";
+              '';
 
-          postFixup = ''
-            makeWrapper $out/bin/run.fish $out/bin/caelestia-shell \
-            --set QT_QUICK_CONTROLS_STYLE Basic \
-            --set CAELESTIA_BD_PATH "$out/bin/beat_detector" \
-            --prefix PATH : ${pkgs.lib.makeBinPath deps} \
-            --prefix FONTCONFIG_PATH : ${pkgs.fontconfig.out}/etc/fonts
-          '';
+              dependencies = deps;
+            };
+          default = pkgs.buildEnv {
+            name = "caelestia";
+            paths = [
+              self.packages.${system}.shell
+              self.packages.${system}.cli
+            ];
+          };
         };
       }
     );
